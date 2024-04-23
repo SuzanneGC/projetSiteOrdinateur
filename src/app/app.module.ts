83fbdaf61c147a3828e128b54c8c115fdd794208
Suzanne G-C <suzanne.grelet@gmail.com>
2024-04-23 11:24:59 +0200

    Première version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HomePageComponent } from './home-page/home-page.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    QuestionnaireComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
